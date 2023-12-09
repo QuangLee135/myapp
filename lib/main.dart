84b46db58f31c28a9a265af76c246396c12dd242
QuangLee <quanglee5923@fmail.com>
2023-12-09 20:44:11 +0700

    Lab03: Building layouts - Xây dựng bố cục

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,74 @@
 import 'package:flutter/material.dart';
 
-class CounterDisplay extends StatelessWidget {
-  const CounterDisplay({required this.count});
-
-  final int count;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Count: $count');
-  }
-}
-
-class CounterIncrementor extends StatelessWidget {
-  const CounterIncrementor({required this.onPressed});
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: onPressed,
-      child: Icon(Icons.add),
-    );
-  }
-}
-
-class CounterDecrementor extends StatelessWidget {
-  const CounterDecrementor({required this.onPressed});
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: onPressed,
-      child: Icon(Icons.remove),
-    );
-  }
-}
-
-class Counter extends StatefulWidget {
-  @override
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  void _increment() => setState(() {
-        ++_counter;
-      });
-
-  void _decrement() => setState(() {
-        --_counter;
-      });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Expanded(child: CounterDecrementor(onPressed: _decrement)),
-        SizedBox(width: 16),
-        CounterDisplay(count: _counter),
-        SizedBox(width: 16),
-        Expanded(child: CounterIncrementor(onPressed: _increment)),
-      ],
-    );
-  }
-}
-
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Counter(),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Thông tin sinh viên',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _maSinhVien = '';
+  String _hoTen = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Thông tin sinh viên'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // Mã sinh viên
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Mã sinh viên',
+              ),
+              onChanged: (value) {
+                _maSinhVien = value;
+              },
+            ),
+            // Họ và tên
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Họ và tên',
+              ),
+              onChanged: (value) {
+                _hoTen = value;
+              },
+            ),
+            // nút Xem thông tin
+            TextButton(
+              onPressed: () {
+                // hiển thị thông tin sinh viên
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      'Mã sinh viên: $_maSinhVien\n'
+                      'Họ và tên: $_hoTen',
+                    ),
+                  ),
+                );
+              },
+              child: Text('Xem thông tin'),
+            ),
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
