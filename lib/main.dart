aafd3dfb21fe96896215167f3c14f4f6c3bf0b3c
QuangLee <quanglee5923@fmail.com>
2023-12-09 20:42:39 +0700

    Lab04: Tạo ứng dụng đếm bấm nút

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,83 @@
 import 'package:flutter/material.dart';
-import 'counter/counter_view.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class CounterDisplay extends StatelessWidget {
+  const CounterDisplay({required this.count});
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final int count;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const CounteView(),
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  const CounterIncrementor({required this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      child: Icon(Icons.add),
     );
   }
+}
+
+class CounterDecrementor extends StatelessWidget {
+  const CounterDecrementor({required this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      child: Icon(Icons.remove),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() => setState(() {
+        ++_counter;
+      });
+
+  void _decrement() => setState(() {
+        --_counter;
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(child: CounterDecrementor(onPressed: _decrement)),
+        SizedBox(width: 16),
+        CounterDisplay(count: _counter),
+        SizedBox(width: 16),
+        Expanded(child: CounterIncrementor(onPressed: _increment)),
+      ],
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Counter(),
+        ),
+      ),
+    ),
+  );
 }
