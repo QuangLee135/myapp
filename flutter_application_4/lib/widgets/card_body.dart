29c167aaff0352da62f17551f43fffba2b09a516
QuangLee <quanglee5923@fmail.com>
2023-12-13 00:03:37 +0700

    Todolist 2

--- flutter_application_4/lib/widgets/card_body.dart
+++ flutter_application_4/lib/widgets/card_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class CardBody extends StatelessWidget {
   CardBody({Key? key, required this.item}) : super(key: key);
   var item;
